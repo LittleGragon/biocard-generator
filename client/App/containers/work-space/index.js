bcb117d9182e57b0318666a81ef55a95e2b45500
littlegragon <705764065@qq.com>
2019-02-03 15:44:07 +0800

    fix: 修正fixed 造成的遮挡

--- client/App/containers/work-space/index.js
+++ client/App/containers/work-space/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = {
+  workspace: {
+    marginTop: 64,
+  },
+};
 class WorkSpace extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div id="workspace">
+      <div id="workspace" className={classes.workspace}>
         {this.props.children}
       </div>
     );
@@ -12,5 +19,6 @@ class WorkSpace extends React.Component {
 }
 WorkSpace.propTypes = {
   children: PropTypes.any.isRequired,
+  classes: PropTypes.object.isRequired,
 };
-export default WorkSpace;
+export default withStyles(styles)(WorkSpace);
