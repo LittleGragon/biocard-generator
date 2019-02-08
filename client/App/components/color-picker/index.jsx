aa5833ea74d2e5d80b22ab06f80f1bf4147a145a
littlegragon <705764065@qq.com>
2019-02-08 11:09:41 +0800

    feat: 提供颜色修改支持

--- client/App/components/color-picker/index.jsx
+++ client/App/components/color-picker/index.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { SketchPicker } from 'react-color';
 
-export default class ColorPicker extends React.Component {
+class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleInitProps = () => {
+    const { color } = this.props;
+    this.setState({
+      color,
+    });
+  }
+  handleChange = (color) => {
+    const { onChange } = this.props;
+    this.setState({
+      color,
+    });
+    onChange(color);
+  }
+  componentDidMount() {
+    this.handleInitProps();
+  }
   render() {
+    const { color } = this.state;
     return (
       <div>
-        <SketchPicker />
+        <SketchPicker color={color} onChangeComplete={this.handleChange} />
       </div>
     );
   }
 }
+ColorPicker.propTypes = {
+  color: PropTypes.string,
+  onChange: PropTypes.func,
+};
+ColorPicker.defaultProps = {
+  color: '#fff',
+  onChange: (color) => {
+    return color;
+  },
+};
+
+export default ColorPicker;
