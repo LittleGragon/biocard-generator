a9a8217a99e77b65c6553ac48247a82abf6e4f43
littlegragon <705764065@qq.com>
2019-02-16 02:05:30 +0800

    feat: 布局优化

---
+++ client/App/components/color-selector/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ColorSelector extends React.Component {
+  render() {
+    return (
+      <div>
+        color selctor;
+      </div>
+    );
+  }
+}
+export default ColorSelector;
