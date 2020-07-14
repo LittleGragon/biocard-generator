f7a6e07f6ebe25b13b0e17e5e38273e303ecfb8f
littlegragon <705764065@qq.com>
2020-07-15 01:03:42 +0800

    chore: init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+  <div>
+    snowpack test
+  </div>
+}
+
+const rootDocument = document.getElementById('app')
+
+render(<App />, rootDocument)
